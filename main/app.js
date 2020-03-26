9721a5b3abf18a2baf4a54baef1ba83d2c692ff1
mszymkowiak <mateusz.szymkowiak@l4a-soft.com>
2020-03-26 08:45:00 +0100

    test

--- main/app.js
+++ main/app.js
@@ -9,6 +9,7 @@ const emailSender = require('./email');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({extended: true}));
+const port=process.env.PORT || 3000;
 
 const dbCredentials = {
     host: "localhost",
@@ -33,7 +34,7 @@ app.get('/rss/', function (req, res) {
 
 app.get('/rss/:rssId', function (req, res) {
 
-    let sql = `SELECT * FROM rss WHERE id=${req.params.rssId};`;
+    let sql = `SELECT * FROM rss WHERE id="${req.params.rssId}";`;
 
     let con = mysql.createConnection(dbCredentials);
 
@@ -48,7 +49,7 @@ app.get('/rss/:rssId', function (req, res) {
 
 app.post('/rss/save', function (req, res) {
 
-    if (req.body.email == null || req.body.title == null || req.body.header == null) {
+    if (req.body.mail == null || req.body.title == null || req.body.header == null) {
         console.log("Failed to add RSS");
 
         res.status(400).send({Error: "Required fields: [ email, title, header ]"});
@@ -56,7 +57,7 @@ app.post('/rss/save', function (req, res) {
 
         console.log("Creating new RSS");
 
-        let sql = "INSERT INTO rss (title,header,description,email) VALUES (" + "'" + req.body.title + "','" + req.body.header + "','" + req.body.description + "','" + req.body.email + "');";
+        let sql = `INSERT INTO rss (title,header,description,email) VALUES ("${req.body.title}","${req.body.header}","${req.body.description}","${req.body.mail}");`;
         let con = mysql.createConnection(dbCredentials);
 
         con.connect(function (err) {
@@ -76,13 +77,13 @@ app.post('/rss/save', function (req, res) {
 
 app.post('/rss/send', function (req, res) {
 
-    if (req.body.email == null || req.body.title == null || req.body.header == null) {
+    if (req.body.mail == null || req.body.title == null || req.body.header == null) {
         console.log("Failed to add RSS");
 
         res.status(400).send({Error: "Required fields: [ email, title, description ]"});
     } else {
 
-        let sql = "INSERT INTO rss (title,header,description,email) VALUES (" + "'" + req.body.title + "','" + req.body.header + "','" + req.body.description + "','" + req.body.email + "');";
+        let sql = "INSERT INTO rss (title,header,description,email) VALUES (" + "'" + req.body.title + "','" + req.body.header + "','" + req.body.description + "','" + req.body.mail + "');";
         let con = mysql.createConnection(dbCredentials);
 
         con.connect(function (err) {
@@ -94,20 +95,16 @@ app.post('/rss/send', function (req, res) {
             sql = `SELECT * FROM rss ORDER BY id DESC LIMIT 1;`;
             con.query(sql, function (err, result) {
                 if (err) throw err;
-                emailSender(req.body.email, req.body.title,  req.body.header,  req.body.description);
+                emailSender(req.body.mail, req.body.title,  req.body.header,  req.body.description);
                     res.status(201).send(result);
             });
         });
-
-
-
-
     }
 });
 
 app.delete('/rss/:rssId', function (req, res) {
 
-    let sql = "DELETE FROM rss WHERE id=" + req.params.rssId + ";";
+    let sql = `DELETE FROM rss WHERE id="${req.params.rssId} ";`;
 
     let con = mysql.createConnection(dbCredentials);
 
@@ -120,9 +117,7 @@ app.delete('/rss/:rssId', function (req, res) {
     });
 });
 
-let server = app.listen(8081, function () {
-    let host = server.address().address;
-    let port = server.address().port;
+let server = app.listen(port, function () {
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log(`Example app listening at ${port}`)
 });
